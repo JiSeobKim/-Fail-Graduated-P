73445edfa59402524eabed536d1d561c6198a266
JiseobKim <jiseob812@gmail.com>
2017-05-31 21:44:30 +0900

    PageView 수정

--- GraduatedProject/PageView.swift
+++ GraduatedProject/PageView.swift
@@ -13,7 +13,7 @@ class PageVC: UIPageViewController, UIPageViewControllerDataSource, UIPageViewCo
     lazy var VCArr: [ UIViewController] = {
         return [self.VCInstance(name: "VC1"), self.VCInstance(name: "VC2")]
     }()
-
+    
     private func VCInstance(name:String) -> UIViewController {
         return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: name)
     }
@@ -43,7 +43,7 @@ class PageVC: UIPageViewController, UIPageViewControllerDataSource, UIPageViewCo
     }
     
     
-
+    
     
     public func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
         guard let viewControllerIndex = VCArr.index(of: viewController) else {
@@ -61,8 +61,8 @@ class PageVC: UIPageViewController, UIPageViewControllerDataSource, UIPageViewCo
         
         return VCArr[previousIndex]
     }
-
-
+    
+    
     public func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
         guard let viewControllerIndex = VCArr.index(of: viewController) else {
             return nil
@@ -79,25 +79,25 @@ class PageVC: UIPageViewController, UIPageViewControllerDataSource, UIPageViewCo
         
         return VCArr[nextIndex]
     }
-
-
-
+    
+    
+    
     public func presentationCount(for pageViewController: UIPageViewController) -> Int {
         return VCArr.count
         
     }
-
-
+    
+    
     public func presentationIndex(for pageViewController: UIPageViewController) -> Int {
         guard let firstViewController = viewControllers?.first,
             let firstViewControllerIndex = VCArr.index(of: firstViewController) else{
-            return 0
+                return 0
         }
         return firstViewControllerIndex
     }
     
-
-
-
+    
+    
+    
 }
 
