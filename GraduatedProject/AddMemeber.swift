2b06838de0ffeb948360a456c43b55561d154e2c
JiseobKim <jiseob812@gmail.com>
2017-06-01 23:25:17 +0900

    1. 네비게이션바 추가 2. 구성원 연결 3. Add 연결 4. 네비바 반응형

---
+++ GraduatedProject/AddMemeber.swift
@@ -0,0 +1,16 @@
+//
+//  AddMemeber.swift
+//  GraduatedProject
+//
+//  Created by 김지섭 on 2017. 6. 1..
+//  Copyright © 2017년 JiseobKim. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddMember : UIViewController {
+    
+    
+    
+}
